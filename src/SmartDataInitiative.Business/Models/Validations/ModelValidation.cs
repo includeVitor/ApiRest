4e733771ae7b612ff5e57c5e2fbdad7b46d93735
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-07 16:31:36 -0300

    adding ModelValidation & ReportModelValidation

---
+++ src/SmartDataInitiative.Business/Models/Validations/ModelValidation.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartDataInitiative.Business.Models
+{
+    public class ModelValidation : AbstractValidator<Model>
+    {
+        public ModelValidation()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(3, 150).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            RuleFor(p => p.Name)
+               .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+               .Length(3, 2000).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+        }
+    }
+}
