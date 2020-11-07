62895191dfa344b1a5a3fb76319d72db1b5a9699
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-07 18:45:33 -0300

    Repository

--- src/SmartDataInitiative.Data/Repository/Repository.cs
+++ src/SmartDataInitiative.Data/Repository/Repository.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SmartDataInitiative.Business.Interfaces;
+using SmartDataInitiative.Business.Models;
 using SmartDataInitiative.Data.Context;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SmartDataInitiative.Data.Repository
 {
@@ -17,34 +22,34 @@ namespace SmartDataInitiative.Data.Repository
             DbSet = db.Set<TEntity>();
         }
 
-        public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+        public async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate)
         {
             return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
         }
 
-        public virtual async Task<TEntity> ObterPorId(Guid id)
+        public virtual async Task<TEntity> GetById(Guid id)
         {
             return await DbSet.FindAsync(id);
         }
 
-        public virtual async Task<List<TEntity>> ObterTodos()
+        public virtual async Task<List<TEntity>> All()
         {
             return await DbSet.ToListAsync();
         }
 
-        public virtual async Task Adicionar(TEntity entity)
+        public virtual async Task Add(TEntity entity)
         {
             DbSet.Add(entity);
             await SaveChanges();
         }
 
-        public virtual async Task Atualizar(TEntity entity)
+        public virtual async Task Update(TEntity entity)
         {
             DbSet.Update(entity);
             await SaveChanges();
         }
 
-        public virtual async Task Remover(Guid id)
+        public virtual async Task Delete(Guid id)
         {
             DbSet.Remove(new TEntity { Id = id });
             await SaveChanges();
@@ -60,3 +65,4 @@ namespace SmartDataInitiative.Data.Repository
             Db?.Dispose();
         }
     }
+}
