52a6839c90085e4a7de2e190997046085606de33
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-07 17:08:13 -0300

    adding ReportModelMapping

---
+++ src/SmartDataInitiative.Data/Mappings/ReportModelMapping.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartDataInitiative.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartDataInitiative.Data.Mappings
+{
+    public class ReportModelMapping : IEntityTypeConfiguration<ReportModel>
+    {
+        public void Configure(EntityTypeBuilder<ReportModel> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasColumnType("varchar(150)");
+
+            builder.Property(p => p.Description)
+                .IsRequired()
+                .HasColumnType("varchar(2000)");
+
+            builder.ToTable("ReportModels");
+        }
+    }
+}
