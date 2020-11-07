503271ef800d4461313f89826437f7b3c2499101
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-07 19:16:29 -0300

    Implementing the interface projects in a project repository

---
+++ src/SmartDataInitiative.Business/Interfaces/IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using SmartDataInitiative.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDataInitiative.Business.Interfaces
+{
+    public interface IProjectRepository : IRepository<Project>
+    {
+        Task<Project> GetAllFields(Guid id);
+        Task<Project> GetAllReportModels(Guid id);
+    }
+}
---
+++ src/SmartDataInitiative.Data/Repository/ProjectRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartDataInitiative.Business.Interfaces;
+using SmartDataInitiative.Business.Models;
+using SmartDataInitiative.Data.Context;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartDataInitiative.Data.Repository
+{
+    public class ProjectRepository : Repository<Project>, IProjectRepository
+    {
+        public ProjectRepository(MyDbContext context) : base (context) {}
+
+        public async Task<Project> GetAllFields(Guid id)
+        {
+            return await 
+                    Db.Projects.AsNoTracking()
+                    .Include(c => c.Fields)
+                    .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Project> GetAllReportModels(Guid id)
+        {
+            return await
+                    Db.Projects.AsNoTracking()
+                    .Include(c => c.ReportModels)
+                    .FirstOrDefaultAsync(c => c.Id == id);
+        }
+    }
+}
