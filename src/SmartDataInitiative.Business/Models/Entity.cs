638c06301dbb5ff55a61ca8e0fa2696412853847
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-07 15:06:56 -0300

    Creating folder model and entity abstract class, project model

---
+++ src/SmartDataInitiative.Business/Models/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartDataInitiative.Business.Models
+{
+    abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+    }
+}
