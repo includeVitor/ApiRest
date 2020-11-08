2b708a9ea4bcdafc446a1d3670ea5ee720ba76e2
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-08 11:38:45 -0300

    Creating project interface && implementing project interface

---
+++ src/SmartDataInitiative.Business/Interfaces/Services/IProjectService.cs
@@ -0,0 +1,17 @@
+﻿using SmartDataInitiative.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDataInitiative.Business.Interfaces.Services
+{
+    public interface IProjectService : IDisposable
+    {
+        Task<bool> Add(Project project);
+        Task<bool> Update(Project project);
+        Task<bool> Remove(Project project);
+        Task<bool> SaveField(Project project);
+        Task<bool> SaveReportModel(Project project);
+    }
+}
--- src/SmartDataInitiative.Business/Services/ProjectService.cs
+++ src/SmartDataInitiative.Business/Services/ProjectService.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using SmartDataInitiative.Business.Interfaces;
+using SmartDataInitiative.Business.Interfaces.Services;
+using SmartDataInitiative.Business.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SmartDataInitiative.Business.Services
 {
-    public class ProjectService
+    public class ProjectService : IProjectService
     {
+        public async Task<bool> Add(Project project)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<bool> Remove(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveField(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveReportModel(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
