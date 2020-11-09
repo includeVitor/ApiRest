7006d8cff860669bf419dcd8db14667b18bc4406
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-09 12:53:25 -0300

    implementing projects controller

--- src/SmartDataInitiative.Api/v1/Controllers/ProjectsController.cs
+++ src/SmartDataInitiative.Api/v1/Controllers/ProjectsController.cs
@@ -44,6 +44,7 @@ namespace SmartDataInitiative.Api.v1.Controllers
             return project;
         }
 
+        [HttpPost]
         public async Task<ActionResult<ProjectViewModel>> Add(ProjectViewModel projectViewModel)
         {
             if (!ModelState.IsValid) return FormattedResponse(ModelState);
@@ -53,111 +54,36 @@ namespace SmartDataInitiative.Api.v1.Controllers
             return FormattedResponse(projectViewModel);
         }
 
+        [HttpPut]
+        public async Task<ActionResult<ProjectViewModel>> Update(Guid id, ProjectViewModel projectViewModel)
+        {
+            if(id == projectViewModel.Id)
+            {
+                NotifyError("Id incorreto");
+                return FormattedResponse(projectViewModel);
+            }
 
+            if (!ModelState.IsValid) return FormattedResponse(ModelState);
 
+            await _projectService.Update(_mapper.Map<Project>(projectViewModel));
 
+            return FormattedResponse(projectViewModel);
+        }
 
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProjectViewModel>> Remove(Guid id)
+        {
+            var projectViewModel = await GetProject(id);
 
+            if (projectViewModel == null) return NotFound();
+
+            await _projectService.Remove(id);
+
+            return FormattedResponse(projectViewModel);
+
+        }
 
         private async Task<Project> GetProject(Guid id) => _mapper.Map<Project>(await _projectService.Show(id));
-
-
-
-
-
-
-        //private readonly MyDbContext _context;
-
-        //public ProjectsController(MyDbContext context)
-        //{
-        //    _context = context;
-        //}
-
-        //// GET: api/Projects
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
-        //{
-        //    return await _context.Projects.ToListAsync();
-        //}
-
-        //// GET: api/Projects/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Project>> GetProject(Guid id)
-        //{
-        //    var project = await _context.Projects.FindAsync(id);
-
-        //    if (project == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return project;
-        //}
-
-        //// PUT: api/Projects/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
-        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutProject(Guid id, Project project)
-        //{
-        //    if (id != project.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(project).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!ProjectExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
-
-        //// POST: api/Projects
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
-        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        //[HttpPost]
-        //public async Task<ActionResult<Project>> PostProject(Project project)
-        //{
-        //    _context.Projects.Add(project);
-        //    await _context.SaveChangesAsync();
-
-        //    return CreatedAtAction("GetProject", new { id = project.Id }, project);
-        //}
-
-        //// DELETE: api/Projects/5
-        //[HttpDelete("{id}")]
-        //public async Task<ActionResult<Project>> DeleteProject(Guid id)
-        //{
-        //    var project = await _context.Projects.FindAsync(id);
-        //    if (project == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    _context.Projects.Remove(project);
-        //    await _context.SaveChangesAsync();
-
-        //    return project;
-        //}
-
-        //private bool ProjectExists(Guid id)
-        //{
-        //    return _context.Projects.Any(e => e.Id == id);
-        //}
 
     }
 }
