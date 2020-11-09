84aac8a0b8cbe0e97d47c04d374c865a6a875f64
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-09 06:22:19 -0300

    adding Feedback interface

---
+++ src/SmartDataInitiative.Business/Interfaces/Services/IFeedbackService.cs
@@ -0,0 +1,17 @@
+﻿using SmartDataInitiative.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDataInitiative.Business.Interfaces.Services
+{
+    public interface IFeedbackService : IDisposable
+    {
+        Task<IEnumerable<Feedback>> All();
+        Task<Feedback> Show(Guid id);
+        Task<bool> Add(Feedback feedback);
+        Task<bool> Update(Feedback feedback);
+        Task<bool> Remove(Guid id);
+    }
+}
