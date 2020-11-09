a653bd4f838c33e26a8d7fe8c744c8fba0cf8074
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-09 00:58:42 -0300

    adding IReportModelService

---
+++ src/SmartDataInitiative.Business/Interfaces/Services/IReportModelService.cs
@@ -0,0 +1,17 @@
+﻿using SmartDataInitiative.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDataInitiative.Business.Interfaces.Services
+{
+    public interface IReportModelService : IDisposable
+    {
+        Task<IEnumerable<ReportModel>> All();
+        Task<ReportModel> Show(Guid id);
+        Task<bool> Add(ReportModel reportModel);
+        Task<bool> Update(ReportModel reportModel);
+        Task<bool> Remove(Guid id);
+    }
+}
