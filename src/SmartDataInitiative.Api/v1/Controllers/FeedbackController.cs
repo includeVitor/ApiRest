8aee5e028f7e2f164a6128fa264dbbae6fd8d6fe
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-10 12:56:18 -0300

    adding FeedbackController && FeedbackViewModel

---
+++ src/SmartDataInitiative.Api/v1/Controllers/FeedbackController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartDataInitiative.Business.Interfaces;
+using SmartDataInitiative.Business.Interfaces.Services;
+
+namespace SmartDataInitiative.Api.v1.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class FeedbackController : MainController
+    {
+        private readonly IFeedbackService _feedbackService;
+        private readonly IMapper _mapper;
+
+
+        public FeedbackController(INotify notify,
+                                  IFeedbackService feedbackService, 
+                                  IMapper mapper) : base(notify)
+        {
+            _feedbackService = feedbackService;
+            _mapper = mapper;
+        }
+
+
+
+
+
+    }
+}
