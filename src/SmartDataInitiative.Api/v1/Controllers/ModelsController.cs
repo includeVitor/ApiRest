d44492bdff3813613e0f8507838d2d971ecb49b8
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-10 12:04:15 -0300

    adding GetModel to the controller

--- src/SmartDataInitiative.Api/v1/Controllers/ModelsController.cs
+++ src/SmartDataInitiative.Api/v1/Controllers/ModelsController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SmartDataInitiative.Api.ViewModels;
 using SmartDataInitiative.Business.Interfaces;
 using SmartDataInitiative.Business.Interfaces.Services;
 
@@ -15,15 +17,24 @@ namespace SmartDataInitiative.Api.v1.Controllers
     public class ModelsController : MainController
     {
         private readonly IModelService _modelService;
+        private readonly IMapper _mapper;
 
-
-        public ModelsController(INotify notify, 
-                                IModelService modelService) : base(notify)
+        public ModelsController(INotify notify,
+                                IModelService modelService, 
+                                IMapper mapper) : base(notify)
         {
             _modelService = modelService;
+            _mapper = mapper;
         }
 
-        
+
+
+
+
+
+
+
+        public async Task<ModelViewModel> GetModel(Guid id) => _mapper.Map<ModelViewModel>(await _modelService.Show(id));
 
 
 
