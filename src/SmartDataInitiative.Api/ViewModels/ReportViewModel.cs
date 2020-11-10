e2b6a52d73eb3ddb13b6a802299159a07fc0ffdb
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-10 12:25:37 -0300

    adding ReportViewModel

---
+++ src/SmartDataInitiative.Api/ViewModels/ReportViewModel.cs
@@ -0,0 +1,26 @@
+﻿using SmartDataInitiative.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartDataInitiative.Api.ViewModels
+{
+    public class ReportViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTime InitialDate { get; set; }
+
+        [Required]
+        public DateTime FinalDate { get; set; }
+
+        public ReportModel ReportModel { get; set; }
+        public Field Field { get; set; }
+
+
+    }
+}
---
+++ src/SmartDataInitiative.Api/v1/Controllers/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartDataInitiative.Business.Interfaces;
+using SmartDataInitiative.Business.Interfaces.Services;
+
+namespace SmartDataInitiative.Api.v1.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ReportController : MainController
+    {
+        private readonly IReportService _reportService;
+        private readonly IMapper _mapper;
+
+        public ReportController(INotify notify, 
+                                IReportService reportService, 
+                                IMapper mapper) : base(notify)
+        {
+            _reportService = reportService;
+            _mapper = mapper;
+        }
+
+
+        public async Task<ReportViewModel> GetReport(Guid id) => _mapper.Map<ReportViewModel>(await _reportService.Show(id));
+    }
+}
