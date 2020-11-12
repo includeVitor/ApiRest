8e6552543aa2a4b527df8cafc08829ae2c5056c7
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-12 17:37:27 -0300

    adding IdentityDbContext

---
+++ src/SmartDataInitiative.Api/Configuration/IdentityConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartDataInitiative.Api.Configuration
+{
+    public static class IdentityConfig
+    {
+
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services,
+                                                                       IConfiguration configuration)
+        {
+
+
+
+            return services;
+        }
+
+
+    }
+}
---
+++ src/SmartDataInitiative.Api/Context/IdentityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartDataInitiative.Api.Context
+{
+    public class IdentityDbContext :DbContext
+    {
+
+
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options) {}
+    }
+}
