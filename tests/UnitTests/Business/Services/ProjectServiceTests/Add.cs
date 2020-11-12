cd763784ef332a767fe2f5cdecffc95733e822ed
Vitor Hugo <vitorzcomputacao@gmail.com>
2020-11-12 17:22:43 -0300

    Getting started with unit tests

--- tests/UnitTests/Business/Services/ProjectServiceTests/Add.cs
+++ tests/UnitTests/Business/Services/ProjectServiceTests/Add.cs
@@ -1,5 +1,6 @@
 using Moq;
 using SmartDataInitiative.Business.Interfaces;
+using SmartDataInitiative.Business.Interfaces.Services;
 using SmartDataInitiative.Business.Models;
 using System;
 using System.Threading.Tasks;
@@ -26,41 +27,42 @@ namespace UnitTests
 
         private readonly Mock<IRepository<Project>> _mockProjectRepository;
 
+
         public Add()
         {
             _mockProjectRepository = new Mock<IRepository<Project>>();
         }
 
         [Fact]
-        public async Task ExecuteValidation()
+        public async Task Add_ExecuteValidation()
         {
+            // arrange
             var project = new Project
             {
-                Name = "Project test",
-                Description = "Project description",
-                InitialDate = new System.DateTime(2020, 05, 20, 8, 0, 0),
-                FinalDate = new System.DateTime(2021, 05, 20, 8, 0, 0),
-                Status = Status.NotReady
+                Name = _name,
+                Description = _description,
+                InitialDate = _initialDate,
+                FinalDate = _finalDate,
+                Status = _status
             };
 
+            Assert.Equal(_name, project.Name);
+            Assert.Equal(_description, project.Description);
+            Assert.Equal(_initialDate, project.InitialDate);
+            Assert.Equal(_finalDate, project.FinalDate);
+            Assert.Equal(_status, project.Status);
+
+            _mockProjectRepository.Setup(x => x.Add(project)).Returns(Task.FromResult(project));
+
+            //act
+            //await _projectService.Add(project);
+
+            //_mockProjectRepository.Verify(x => x.Add(It.IsAny<Project>()), Times.Once);
 
 
-
-
-
-
-
-
+            //assert
 
 
         }
-
-
-
-
-        //bool isOdd(int value)
-        //{
-        //    return value % 2 == 1;
-        //}
     }
 }
